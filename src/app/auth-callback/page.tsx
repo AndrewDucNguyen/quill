d0e3ab9a6238e5d829600f8f531a113b06c5779c
Andrew Nguyen <anguyen0615@gmail.com>
2023-11-07 20:51:05 -0600

    Setup Kinde routing and database
    - Finish setting up Kinde routing and authentication
    - Installed packages for database
    - Setup trpc

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,14 +1,22 @@
 import { useRouter, useSearchParams } from 'next/navigation'
 import React from 'react'
+import { trpc } from '../_trpc/client';
 
 const page = () => {
     const router = useRouter();
 
     const searchParams = useSearchParams();
-    const origin = searchParams.get('origin')
-  return (
-    <div>page</div>
-  )
+
+    const origin = searchParams.get('origin');
+
+    const {data, isLoading} = trpc.authCallback.useQuery(undefined, {
+      onSuccess: ({success}) => {
+        if(success){
+          //user is synced to db
+          router.push(origin ? `/${origin}` : '/dashboard')
+        }
+      }
+    })
 }
 
 export default page
