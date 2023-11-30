376b60d982dda53e42d55fc424c98ba6abc1bb27
Andrew Nguyen <anguyen0615@gmail.com>
2023-11-29 21:26:57 -0600

    Implement loading state of chat
    - Implement success/failure state for chat

--- src/components/chat/ChatWrapper.tsx
+++ src/components/chat/ChatWrapper.tsx
@@ -4,7 +4,9 @@ import React from 'react'
 import Messages from './Messages'
 import ChatInput from './ChatInput'
 import { trpc } from '@/app/_trpc/client'
-import { Loader2 } from 'lucide-react'
+import { ChevronLeft, Loader2, XCircle } from 'lucide-react'
+import Link from 'next/link'
+import { buttonVariants } from '../ui/button'
 
 interface ChatWrapperProps {
     fileId: string
@@ -45,11 +47,26 @@ const ChatWrapper = ({ fileId }: ChatWrapperProps) => {
             <ChatInput isDisabled />
         </div>
     )
+
+    if (data?.status === "FAILED") return (
+        <div className='relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2'>
+            <div className='flex-1 flex justify-center items-center flex-col mb-28'>
+                <div className='flex flex-col items-center gap-2'>
+                    <XCircle className='h-8 w-8 text-red-500' />
+                    <h3 className='font-semibold text-xl'>Too many pages in PDF...</h3>
+                    <p className='text-zinc-500 text-sm'>Your <span className='font-medium'>Free</span> plan supports up to 5 pages per PDF.</p>
+                    <Link href='/dashboard' className={buttonVariants({ variant: 'secondary', className: "mt-4" })}><ChevronLeft className='h-3 w-3 mr-1.5' />Back</Link>
+                </div>
+            </div>
+            <ChatInput isDisabled />
+        </div>
+    )
     return (
         <div className='relative min-h-full bg-zinc-50 divide-y flex flex-col divide-zinc-200 justify-between gap-2'>
             <div className='flex-1 justify-between flex flex-col mb-28'>
                 <Messages />
             </div>
+            <ChatInput />
         </div>
     )
 }
