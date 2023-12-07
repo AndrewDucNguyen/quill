f8d96f062eada769ec47544d7afb663b97b3c0b6
Andrew Nguyen <anguyen0615@gmail.com>
2023-12-06 18:22:30 -0600

    Finish displaying message

--- src/components/chat/ChatWrapper.tsx
+++ src/components/chat/ChatWrapper.tsx
@@ -69,7 +69,7 @@ const ChatWrapper = ({ fileId }: ChatWrapperProps) => {
         <ChatContextProvider fileId={fileId}>
             <div className='relative min-h-full bg-zinc-50 divide-y flex flex-col divide-zinc-200 justify-between gap-2'>
                 <div className='flex-1 justify-between flex flex-col mb-28'>
-                    <Messages />
+                    <Messages fileId={fileId} />
                 </div>
                 <ChatInput />
             </div>
--- src/components/chat/Messages.tsx
+++ src/components/chat/Messages.tsx
@@ -3,6 +3,7 @@ import { INFINITE_QUERY_LIMIT } from '@/config/infinte-query'
 import { Loader2, MessageSquare } from 'lucide-react'
 import React from 'react'
 import Skeleton from 'react-loading-skeleton'
+import Message from './Message'
 
 interface MessagesProps {
   fileId: string
@@ -38,32 +39,31 @@ const Messages = ({ fileId }: MessagesProps) => {
 
   return (
     <div className='flex max-h-[calc(100vh)-3.5rem-7rem] border-zinc-200 flex-1 flex-col-reverse gap-4 p-3 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch'>
-      {/* {combinedMessages && combinedMessages.length > 0 ? (
+      {combinedMessages && combinedMessages.length > 0 ? (
         combinedMessages.map((message, i) => {
 
           const isNextMessageSamePerson = combinedMessages[i - 1]?.isUserMessage === combinedMessages[i]?.isUserMessage
 
           if (i === combinedMessages.length - 1) {
-            // return <Message />
+            return <Message message={message} isNextMessageSamePerson={isNextMessageSamePerson} key={message.id} />
           } else return (
-            // <Message />
+            <Message message={message} isNextMessageSamePerson={isNextMessageSamePerson} key={message.id} />
           )
-        }
         })
       ) : isLoading ? (
-      <div className='w-full flex flex-col gap-2'>
-        <Skeleton className='h-16' />
-        <Skeleton className='h-16' />
-        <Skeleton className='h-16' />
-        <Skeleton className='h-16' />
-      </div>
+        <div className='w-full flex flex-col gap-2'>
+          <Skeleton className='h-16' />
+          <Skeleton className='h-16' />
+          <Skeleton className='h-16' />
+          <Skeleton className='h-16' />
+        </div>
       ) : (
-      <div className='flex-1 flex flex-col items-center justify-center gap-2'>
-        <MessageSquare className='h-8 w-8 text-blue-500' />
-        <h3 className='font-semibold text-xl'>You&apos;re all set!</h3>
-        <p className='text-zinc-500 text-sm'>Ask your first question to get started.</p>
-      </div>
-      )} */}
+        <div className='flex-1 flex flex-col items-center justify-center gap-2'>
+          <MessageSquare className='h-8 w-8 text-blue-500' />
+          <h3 className='font-semibold text-xl'>You&apos;re all set!</h3>
+          <p className='text-zinc-500 text-sm'>Ask your first question to get started.</p>
+        </div>
+      )}
     </div>
   )
 }
