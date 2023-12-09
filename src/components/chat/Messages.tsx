351e7afa289d20fb97ac2457dcb94f6913e9403d
Andrew Nguyen <anguyen0615@gmail.com>
2023-12-08 23:28:58 -0600

    Optimistic updates for chat

--- src/components/chat/Messages.tsx
+++ src/components/chat/Messages.tsx
@@ -1,15 +1,18 @@
 import { trpc } from '@/app/_trpc/client'
 import { INFINITE_QUERY_LIMIT } from '@/config/infinte-query'
 import { Loader2, MessageSquare } from 'lucide-react'
-import React from 'react'
+import React, { useContext } from 'react'
 import Skeleton from 'react-loading-skeleton'
 import Message from './Message'
+import { ChatContext } from './ChatContext'
 
 interface MessagesProps {
   fileId: string
 }
 
 const Messages = ({ fileId }: MessagesProps) => {
+
+  const { isLoading: isAiThinking } = useContext(ChatContext)
 
   const { data, isLoading, fetchNextPage } = trpc.getFileMessages.useInfiniteQuery({
     fileId,
@@ -33,7 +36,7 @@ const Messages = ({ fileId }: MessagesProps) => {
   }
 
   const combinedMessages = [
-    ...(true ? [loadingMessage] : []),
+    ...(isAiThinking ? [loadingMessage] : []),
     ...messages ?? []
   ]
 
